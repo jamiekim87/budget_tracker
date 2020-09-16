5b1f0f7fbca088d431cb3a37bfc5bfe1bb1b597b
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-09-15 20:21:11 -0700

    git push

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,66 +1,53 @@
-const FILES_TO_CACHE = [
-    "/", "/index.html","index.js", "/db.js", "/style.css"];
-
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
-// install
-self.addEventListener("install", function(evt) {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("Your files were pre-cached successfully!");
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-
-  self.skipWaiting();
-});
-
-// activate
-self.addEventListener("activate", function(evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('budget-cache-v1').then(cache => {
+        console.log('Opened cache')
+        return cache.addAll([
+          '/',
+          '/icons/icon/-192x192.png',
+          '/icons/icon/-512x512.png',
+          '/db.js'
+          '/index.html',
+          '/index.js',
+          '/manifest.json',
+          '/service-worker.js',
+          '/styles.css'
+          
+        ])
+      })
+    )
+  })
+  
+  self.addEventListener('fetch', event => {
+  
+    if (event.request.url.includes('/api/')) {
+      // event.respondWith(
+      //   caches.open('data-cache-v1').then(cache => {
+      //     return fetch(event.request)
+      //       .then(res => {
+      //         if (res.status === 200) {
+      //           cache.put(event.request.url, res.clone())
+      //         }
+      //       })
+      //       .catch(err => {
+      //         return cache.match(event.request)
+      //       })
+      //   })
+      //   .catch(err => console.error(err))
+      // )
+      return
+    }
+  
+    event.respondWith(
+      fetch(event.request).catch(err => {
+        return caches.match(event.request).then(res => {
+          if (res) {
+            return res
+          } else if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('/')
           }
         })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
-
-// fetch
-self.addEventListener("fetch", evt => {
-    if(evt.request.url.includes('/api/')) {
-        console.log('[Service Worker] Fetch(data)', evt.request.url);
-    
-evt.respondWith(
-                caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(evt.request)
-                .then(response => {
-                    if (response.status === 200){
-                        cache.put(evt.request.url, response.clone());
-                    }
-                    return response;
-                })
-                .catch(err => {
-                    return cache.match(evt.request);
-                });
-            })
-            );
-            return;
-        }
-
-evt.respondWith(
-    caches.open(CACHE_NAME).then( cache => {
-      return cache.match(evt.request).then(response => {
-        return response || fetch(evt.request);
-      });
-    })
-  );
-});
+      })
+    )
+  
+  })
